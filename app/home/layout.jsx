29e5329ad4a163e4404787f16641505b323088a5
Leinier Caraballo Yanes <94509566+leiniercy@users.noreply.github.com>
2023-12-06 23:49:46 -0500

    anadi la informacion de la biblioteca aos

--- app/home/layout.jsx
+++ app/home/layout.jsx
@@ -1,8 +1,19 @@
+"use client"
 
+import React,{ useEffect } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import BarraMenu from '@components/BarraMenu';
 
 const HomeLayout = ({ children }) => {
+
+    useEffect(() => {
+        AOS.init({
+            delay: 1, // values from 0 to 3000, with step 50ms
+            duration: 1500, // values from 0 to 3000, with step 50ms
+        })
+    }, [])
 
     return (<>
         <BarraMenu />
