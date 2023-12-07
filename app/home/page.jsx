64d088bc76e9a0d91306960dd67868a8ff4a022c
Leinier Caraballo Yanes <94509566+leiniercy@users.noreply.github.com>
2023-12-06 23:58:08 -0500

    arregle el efecto de la libreria on scroll  en  la pagina

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,11 +1,19 @@
-'use client'
+"use client"
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import React, { useState, useEffect } from 'react';
 import CustomCarrusel from '@components/CustomCarrusel';
 import CustomAreaSindical from '@components/CustomAreaSindical';
 
 export default function Home() {
 
+    useEffect(() => {
+        AOS.init({
+            delay: 1, // values from 0 to 3000, with step 50ms
+            duration: 1500, // values from 0 to 3000, with step 50ms
+        })
+    }, [])
 
 
 return (
--- components/CustomAreaSindical.jsx
+++ components/CustomAreaSindical.jsx
@@ -4,24 +4,20 @@ const ZonaSindical = () => {
     return (
         <div className="border-1 surface-border border-round m-2 text-center py-5 px-3">
             <div className="mb-3">
-                <img src={'./imagenes/ilustracion-plana-concepto-comunicacion-remota_720185-633.jpg'} alt='' className="w-6 shadow-2" />
+                <img src={'/assets/image/ilustracion-plana-concepto-comunicacion-remota_720185-633.jpg'} alt='' className="w-6 shadow-2" />
             </div>
             <div>
-                <div className="mt-5 flex flex-wrap gap-2 justify-content-center">             
-              <div data-aos="zoom-in-down"></div>
-                    <img src={'./imagenes/ilustracion-plana-concepto-comunicacion-remota_720185-633.jpg'} alt='' className="w-6 shadow-2" />
+                <div data-aos="zoom-in-down" className="mt-5 flex flex-wrap gap-2 justify-content-center">             
+                    <img src={'/assets/image/ilustracion-plana-concepto-comunicacion-remota_720185-633.jpg'} alt='' className="w-6 shadow-2" />
                 </div>
-                <div className="mt-5 flex flex-wrap gap-2 justify-content-center">
-                <div data-aos="zoom-in-left"></div>
-                    <img src={'./imagenes/ilustracion-plana-concepto-comunicacion-remota_720185-633.jpg'} alt='' className="w-6 shadow-2" />
+                <div data-aos="zoom-in-left" className="mt-5 flex flex-wrap gap-2 justify-content-center">
+                    <img src={'/assets/image/ilustracion-plana-concepto-comunicacion-remota_720185-633.jpg'} alt='' className="w-6 shadow-2" />
                 </div>
-                <div className="mt-5 flex flex-wrap gap-2 justify-content-center">
-                <div data-aos="flip-left"></div>
-                    <img src={'./imagenes/ilustracion-plana-concepto-comunicacion-remota_720185-633.jpg'} alt='' className="w-6 shadow-2" />
+                <div data-aos="flip-left" className="mt-5 flex flex-wrap gap-2 justify-content-center">
+                    <img src={'/assets/image/ilustracion-plana-concepto-comunicacion-remota_720185-633.jpg'} alt='' className="w-6 shadow-2" />
                 </div>
-                <div className="mt-5 flex flex-wrap gap-2 justify-content-center">
-                <div data-aos="flip-right"></div>
-                 <img src={'./imagenes/ilustracion-plana-concepto-comunicacion-remota_720185-633.jpg'} alt='' className="w-6 shadow-2" />
+                <div  data-aos="flip-right" className="mt-5 flex flex-wrap gap-2 justify-content-center">
+                 <img src={'/assets/image//ilustracion-plana-concepto-comunicacion-remota_720185-633.jpg'} alt='' className="w-6 shadow-2" />
                 </div>
             </div>
         </div>
