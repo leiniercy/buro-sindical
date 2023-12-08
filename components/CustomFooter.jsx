5ad21d4dcb0c30ced9f68a78effec8a00fd74be3
Ariel Alarcón Jorge <113232713+mykeflow@users.noreply.github.com>
2023-12-08 00:21:47 -0500

    algo aqui
    hay que arreglar cosas

--- components/CustomFooter.jsx
+++ components/CustomFooter.jsx
@@ -3,24 +3,30 @@ import React from 'react';
 const Siguenos = () => {
 return(
     
-<div class="flex items-end w-full min-h-screen bg-white">
-<h1 class="mt-2 text-center text-sm text-gray-500">Siguenos!</h1>
-    <footer class="w-full text-gray-700 bg-gray-100 body-font">
-        <div
-            class="container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
-            <div class="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left">
-                <a class="flex items-center justify-center font-medium text-gray-900 title-font md:justify-start">
-                <img alt="logo" src="/assets/image/logo-uci.png" height="70" className="mr-2"></img>
-                </a>
+    <div className='relative flex flex-column gap-1 justify-center justify-content-center align-items-center w-full h-full'>
+    <div className="relative flex flex-column gap-1 justify-center justify-content-center align-items-center w-full h-full">
+        <div class="w-20rem">
+            <h1 className='w-full text-center'>Síguenos :</h1>
+        </div></div>
+    <footer class="w-full text-gray-500 bg-gray-200 body-font">
+        <div class="container flex flex-col flex-wrap px-5 py-30 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
+           <div className='relative flex flex-column gap-1 justify-center justify-content-center align-items-center w-full h-full'>
+    <div className="relative flex flex-column gap-1 justify-center justify-content-center align-items-center w-full h-full">
+        <div class="w-18rem">
+                <img alt="logo" src="/assets/image/logo-uci.png" height="100" className="mr-4"></img>
+                </div>
+                </div>
+                <div className='col-12 flex flex-row flex-wrap gap-3'></div>
                 <div class="mt-4">
-                    <span class="inline-flex justify-center mt-2 sm:ml-auto sm:mt-0 sm:justify-start">
+                    <span class="inline-flex justify-left mt-2 sm:ml-auto sm:mt-0 sm:justify-start">
                         <a class="text-gray-500 cursor-pointer hover:text-gray-700">
                             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 class="w-2 h-2" viewBox="0 0 24 24">
                                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                             </svg>
                         </a>
-                        <a class="ml-3 text-gray-500 cursor-pointer hover:text-gray-700">
+                        <div className='col-2 flex flex-row flex-wrap gap-3'></div>
+                        <a class="ml-3 text-gray-500 cursor-pointer hover:text-gray-900">
                             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 class="w-2 h-2" viewBox="0 0 24 24">
                                 <path
@@ -28,6 +34,7 @@ return(
                                 </path>
                             </svg>
                         </a>
+                        <div className='col-2 flex flex-row flex-wrap gap-3'></div>
                         <a class="ml-3 text-gray-500 cursor-pointer hover:text-gray-700">
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                 stroke-width="2" class="w-2 h-2" viewBox="0 0 24 24">
@@ -35,18 +42,20 @@ return(
                                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                             </svg>
                         </a>
+                        <div className='col-2 flex flex-row flex-wrap gap-3'></div>
                         <a class="ml-3 text-gray-500 cursor-pointer hover:text-gray-700">
                             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round"
                                 stroke-linejoin="round" stroke-width="0" class="w-2 h-2" viewBox="0 0 24 24">
                                 <path stroke="none"
                                     d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z">
                                 </path>
-                                <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                                <circle cx="2" cy="2" r="2" stroke="none"></circle>
                             </svg>
                         </a>
                     </span>
                 </div>
             </div>
+            <div className='col-12 flex flex-row flex-wrap gap-3'>
             <div class="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left">
                 <div class="w-full px-4 lg:w-1/4 md:w-1/2">
                     <h2 class="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">About</h2>
@@ -54,9 +63,10 @@ return(
                         <li class="mt-3">
                             <a class="text-gray-500 cursor-pointer hover:text-gray-900">Blog</a>
                         </li>
-                    </nav>
+                    </nav></div>
                 </div>
-                <div class="w-full px-4 lg:w-1/4 md:w-1/2">
+                <div className='col-8 flex flex-row flex-wrap gap-3'>
+                <div class="w-full px-4 lg:w-2/4 md:w-1/2">
                     <h2 class="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">Support</h2>
                     <nav class="mb-10 list-none">
                         <li class="mt-3">
@@ -69,7 +79,8 @@ return(
                             <a class="text-gray-500 cursor-pointer hover:text-gray-900">Atualizaciones</a>
                         </li>
                     </nav>
-                </div>
+                </div></div>
+                <div className='col-6 flex flex-row flex-wrap gap-3'></div>
                 <div class="w-full px-4 lg:w-1/4 md:w-1/2">
                     <h2 class="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">Platform
                     </h2>
