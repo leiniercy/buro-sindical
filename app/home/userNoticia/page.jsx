a68c11e66570994e8f129d143fbfb4b3cde3370d
Ariel Alarcón Jorge <113232713+mykeflow@users.noreply.github.com>
2023-12-07 23:17:46 -0500

    actulizado
    algo

---
+++ app/home/userNoticia/page.jsx
@@ -0,0 +1,197 @@
+'use client'
+import React, { useEffect, useRef, useState } from 'react';
+import CustomFieldset from '@components/CustomFieldset';
+import { Toast } from 'primereact/toast';
+import ActividadService from '@services/ActividadesService';
+import Table from '@components/Actividad/Table';
+import Tools from '@components/Actividad/Tools';
+import DeleteActividadDialog from '@components/Actividad/DeleteActividadDialog';
+import DialogForm from '@components/Actividad/DialogForm';
+
+
+const Actividad = () => {
+
+  let emptyActividad = {
+    id: null,
+    fecha: null,
+    lugar: '',
+    descripcion: '',
+    tipo: '',
+    titulo: ''
+  };
+
+  const emptyFilters = {
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    lugar: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+    descripcion: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+    tipo: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+    titulo: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
+  };/*Mis filtros*/
+
+  const globalFilterFields = [
+    'lugar', 'descripcion', 'tipo', 'titulo'
+  ];
+
+  const toast = useRef(null);
+
+  const [submitted, setSubmitted] = useState(false);
+  const [editActive, setEditActive] = useState(false);
+  const [actividadDialog, setActividadDialog] = useState(false);
+  const [deleteActividadDialog, setDeleteActividadDialog] = useState(false);
+  const [deleteActividadesDialog, setDeleteActividadesDialog] = useState(false);
+
+  const [lugarValid, setLugarValid] = useState(true);
+  const [descripcionValid, setDescripcionValid] = useState(true);
+  const [tipoValid, setTipoValid] = useState(true);
+  const [tituloValid, setTituloValid] = useState(true);
+
+  const [actividad, setActividad] = useState(emptyActividad);
+  const [actividades, setActividades] = useState(null);
+  const [selectedActividades, setSelectedActividades] = useState(null);
+
+  const actividadService = new ActividadService();
+
+  useEffect(() => { }, []);
+
+  const openNew = () => {
+    setSubmitted(false);
+    setActividad(emptyActividad);
+    setActividadDialog(true);
+    setEditActive(false);
+  }; /*Abrir nueva ventana para crear un objeto*/
+
+  const confirmDeleteSelected = () => {
+    if (selectedActividades.length > 1) {
+      setDeleteActividadesDialog(true);
+    }
+    if (selectedActividades.length === 1) {
+      setActividad(selectedActividades[0]);
+      setDeleteActividadDialog(true);
+    }
+  }; /*Abrir el dialog de confirmacion de eliminacion de los objetos*/
+
+  const onSelectionChangeSelectedObjects = (e) => {
+    setSelectedActividades(e.value);
+  } /*Se encarga de obtener la informacion de los objetos seleccionados*/
+
+  const actionBodyTemplate = (rowData) => {
+    return (
+      <React.Fragment>
+        <Button icon="pi pi-pencil" rounded outlined className="mr-2"
+        // onClick={() => edit(rowData)}
+        />
+        <Button icon="pi pi-trash" rounded outlined severity="danger"
+        // onClick={() => confirmDeleteActividad(rowData)}
+        />
+      </React.Fragment>
+    );
+  }; /*Acciones de cada columna de la tabla Update, Delete*/
+
+  const hideDialog = () => {
+    setActividadDialog(false);
+    setSubmitted(false);
+  }; /*Ocultar dialog de anadir*/
+
+  const hideDeleteActividadDialog = () => {
+    setDeleteActividadDialog(false);
+  };/*Ocultar dialog de eliminar un objeto*/
+
+  const hideDeleteActividadesDialog = () => {
+    setDeleteActividadesDialog(false);
+  };/*Ocultar dialog de eliminar varios objetos*/
+
+  const deleteActividad = () => {
+    //modificar utilizando el servicio de consumo a la api   
+    toast.current.show({
+      severity: 'danger',
+      summary: '!Atención',
+      detail: 'Error al eliminar el actividad',
+      life: 2000
+    });
+  };/*Elimnar un Objeto*/
+
+  const deleteSelectedActividades = () => {
+    //eliminar activiades utilizando los servicios de la api
+  };/*Eliminar varios objetos*/
+
+  const validForm = () => {
+
+  } //Validacion de los campos del formulario
+
+  const save = () => {
+
+  }; /*Crear o actualizar la informacion de un objeto*/
+
+  const edit = (actividad) => {
+
+  };/*Editar la informacion de un objeto existente*/
+
+  const onInputTextChange = (e, name) => {
+    const val = (e.target && e.target.value) || '';
+    let _actividad = {...actividad};
+    _actividad[`${name}`] = val;
+    setProduct(_actividad);
+};/*Modifica el valor del nombre del objeto*/
+const onInputNumberChange = (e, name) => {
+    const val = e.value || 0;
+    let _actividad = {...actividad};
+    _actividad[`${name}`] = val;
+    setProduct(_actividad);
+}; /*Modifica el valor de un numero especificado del objeto*/
+
+  return (<>
+    <CustomFieldset label={'Actividades'} icon={'pi-user-edit'}>
+      <div className="col-12">
+        <Tools
+          openNew={openNew}
+          confirmDeleteSelected={confirmDeleteSelected}
+          selectedObjects={selectedActividades}
+        /> {/*barra de herramientas*/}
+      </div>
+      <div className="col-12">
+        <Table
+          headerLabel={'actividades'}
+          objects={actividades}
+          selectedObjects={selectedActividades}
+          setSelectedObject={onSelectionChangeSelectedObjects}
+          emptyFilters={emptyFilters}
+          globalFilterFields={globalFilterFields}
+          actionBodyTemplate={actionBodyTemplate}
+        />
+      </div>
+    </CustomFieldset>
+    <Toast ref={toast} />
+
+    <DialogForm
+      visible={actividadDialog}
+      submitted={submitted}
+      object={actividad}
+      editActive={editActive}
+      hideDialog={hideDialog}
+      save={save}
+      onInputTextChange={onInputTextChange}
+      onInputNumberChange={onInputNumberChange}
+      lugarValid={lugarValid}
+      descripcionValid={descripcionValid}
+      tipoValid={tipoValid}
+      tituloValid={tituloValid}
+    />
+
+    <DeleteActividadDialog
+      visible={deleteActividadDialog}
+      hideDialog={hideDeleteActividadDialog}
+      object={actividad}
+      delete={deleteActividad}
+    />
+
+    <DeleteActividadDialog
+      visible={deleteActividadesDialog}
+      hideDialog={hideDeleteActividadesDialog}
+      object={actividad}
+      delete={deleteSelectedActividades}
+    />
+
+  </>);
+}
+
+export default Actividad;
--- components/CustomAreaSindical.jsx
+++ components/CustomAreaSindical.jsx
@@ -14,8 +14,7 @@ const ZonaSindical = () => {
                     </div>
                     <div class="col-12 lg:col-5 flex flex-column gap-1 px-1s">
                         <h2 class="text-2xl text-gray-900 font-bold md:text-3xl">Bastion Universitario</h2>
-                        <p class="text-gray-600 text-base lg:text-xl">estamos activos!</p>
-                        <p class="text-gray-600 text-base lg:text-xl"> Juntos podemos.</p>
+                        <p class="text-gray-600 text-base lg:text-xl"> Participar en el Bastión Estudiantil Universitario es una oportunidad única para elevar tu preparación patriótica y combativa, y honrar la memoria de los ocho estudiantes de medicina fusilados en 1871. ¡Anímate a participar y aportar tu granito de arena en la defensa de la Revolución y sus conquistas.</p>
                     </div>
                 </div>
             </div>
@@ -24,8 +23,7 @@ const ZonaSindical = () => {
                 <div className='col-12 flex flex-row justify-content-center grid'>
                     <div class="col-5">
                         <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">Actividad deportiva</h2>
-                        <p class="mt-6 text-gray-600">Nuestros jovenes en accion!</p>
-                        <p class="mt-4 text-gray-600"> el futuro continua.</p>
+                        <p class="mt-4 text-gray-600"> Participar en juegos deportivos es una excelente manera de mantenerse activo, mejorar la salud y el bienestar, y hacer nuevos amigos. Además, el deporte ayuda a superar frustraciones, a confiar más en uno mismo, a mejorar la autoestima y a hacerse más fuerte ante las derrotas 1. ¡Anímate a participar y disfruta de los beneficios que el deporte tiene para ofrecer!</p>
                     </div>
                     <div data-aos="fade-down-left" className="col-5">
                         <img className='w-full h-full' src={'/assets/image/ilustracion-plana-concepto-comunicacion-remota_720185-633.jpg'} alt='' />
@@ -40,9 +38,8 @@ const ZonaSindical = () => {
                 <img className='w-full h-full' src={'/assets/image/ilustracion-plana-concepto-comunicacion-remota_720185-633.jpg'} alt='' />
                 </div>
                 <div class="col-5">
-                    <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">Nuxt development is carried out by passionate developers</h2>
-                    <p class="mt-6 text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!</p>
-                    <p class="mt-4 text-gray-600"> Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.</p>
+                    <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">Ciencia y tecnologia</h2>
+                    <p class="mt-4 text-gray-600"> Participar en actividades científicas y tecnológicas es una excelente manera de aprender cosas nuevas, desarrollar habilidades útiles, y contribuir al avance de la ciencia y la tecnología. Además, la ciencia y la tecnología son campos en constante evolución, lo que significa que siempre hay algo nuevo que descubrir y explorar 1. ¡Anímate a participar y descubre todo lo que la ciencia y la tecnología tienen para ofrecer! 1</p>
                 </div>
 
 
